75ea8e34a992d4c353b303f3ba8c8afe9fdeac1f
Marcin Sokołowski <mysy2001@interia.pl>
2021-04-18 20:45:59 +0200

    removed public modifier and @Value annoatation

--- src/main/java/com/mysy2001/hotels/occupancy/domain/rooms/PaymentBasedRoomCategoryProvider.java
+++ src/main/java/com/mysy2001/hotels/occupancy/domain/rooms/PaymentBasedRoomCategoryProvider.java
@@ -1,9 +1,6 @@
 package com.mysy2001.hotels.occupancy.domain.rooms;
 
-import lombok.Value;
-
-@Value
-public class PaymentBasedRoomCategoryProvider implements RoomCategoryProvider<Integer> {
+class PaymentBasedRoomCategoryProvider implements RoomCategoryProvider<Integer> {
 
     static final int PREMIUM_ROOM_MINIMAL_PRICE = 100;
 
