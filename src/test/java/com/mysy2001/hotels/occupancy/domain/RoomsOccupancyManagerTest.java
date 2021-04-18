b50ba5b35617ea28c5041c9e2a89ff5b0be66743
Marcin Sokołowski <mysy2001@interia.pl>
2021-04-18 09:03:19 +0200

    load guests data from json

--- src/test/java/com/mysy2001/hotels/occupancy/domain/RoomsOccupancyManagerTest.java
+++ src/test/java/com/mysy2001/hotels/occupancy/domain/RoomsOccupancyManagerTest.java
@@ -2,14 +2,21 @@ package com.mysy2001.hotels.occupancy.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 class RoomsOccupancyManagerTest {
 
-    private final List<Integer> requestedRoomPrices = List.of(23, 45, 155, 374, 22, 99, 100, 101, 115, 209);
+//    private final List<Integer> requestedRoomPrices = List.of(23, 45, 155, 374, 22, 99, 100, 101, 115, 209);
 
+    private static List<Integer> POTENTIAL_GUESTS;
     private final OccupancyManager objectUnderTest = new OccupancyManager();
 
     private static AvailableRooms createAvailableRooms(final int freePremiumRooms, final int freeEconomyRooms) {
@@ -19,12 +26,19 @@ class RoomsOccupancyManagerTest {
         return availableRooms;
     }
 
+    @BeforeAll
+    static void setUpAll() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream is = RoomsOccupancyManagerTest.class.getResourceAsStream("/potential-guests.json");
+        POTENTIAL_GUESTS = objectMapper.readValue(is, new TypeReference<>() {});
+    }
+
     @Test
     void should_calculate_rooms_occupancy_when_available_are_3_premium_rooms_and_3_economy_rooms() {
 
         final AvailableRooms availableRooms = createAvailableRooms(3, 3);
 
-        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, requestedRoomPrices);
+        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, POTENTIAL_GUESTS);
 
         final OccupancyCalculationResult expected = OccupancyCalculationResult.of(OccupancyDetails.of(3, 738), OccupancyDetails.of(3, 167));
 
@@ -37,7 +51,7 @@ class RoomsOccupancyManagerTest {
 
         final AvailableRooms availableRooms = createAvailableRooms(7, 5);
 
-        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, requestedRoomPrices);
+        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, POTENTIAL_GUESTS);
 
         final OccupancyCalculationResult expected = OccupancyCalculationResult.of(OccupancyDetails.of(6, 1054), OccupancyDetails.of(4, 189));
 
@@ -49,7 +63,7 @@ class RoomsOccupancyManagerTest {
 
         final AvailableRooms availableRooms = createAvailableRooms(2, 7);
 
-        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, requestedRoomPrices);
+        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, POTENTIAL_GUESTS);
 
         final OccupancyCalculationResult expected = OccupancyCalculationResult.of(OccupancyDetails.of(2, 583), OccupancyDetails.of(4, 189));
 
@@ -61,7 +75,7 @@ class RoomsOccupancyManagerTest {
 
         final AvailableRooms availableRooms = createAvailableRooms(7, 1);
 
-        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, requestedRoomPrices);
+        final OccupancyCalculationResult result = objectUnderTest.calculateOccupancy(availableRooms, POTENTIAL_GUESTS);
 
         final OccupancyCalculationResult expected = OccupancyCalculationResult.of(OccupancyDetails.of(7, 1153), OccupancyDetails.of(1, 45));
 
