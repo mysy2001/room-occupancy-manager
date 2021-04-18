2f90ffa3fffb4a47c6192320bfa5095e84f04d76
Marcin Sokołowski <mysy2001@interia.pl>
2021-04-18 13:38:04 +0200

    added test

---
+++ src/test/java/com/mysy2001/hotels/occupancy/domain/PaymentBasedRoomCategoryProviderTest.java
@@ -0,0 +1,40 @@
+package com.mysy2001.hotels.occupancy.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PaymentBasedRoomCategoryProviderTest {
+
+    private PaymentBasedRoomCategoryProvider objectUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        objectUnderTest = new PaymentBasedRoomCategoryProvider();
+    }
+
+    @Test
+    void should_return_premium_category_when_customer_willing_to_pay_more_than_100_EUR() {
+
+        final RoomCategory result = objectUnderTest.getRoomCategory(101);
+
+        assertThat(result).isEqualTo(RoomCategory.PREMIUM);
+    }
+
+    @Test
+    void should_return_premium_category_when_customer_willing_to_pay_100_EUR() {
+
+        final RoomCategory result = objectUnderTest.getRoomCategory(100);
+
+        assertThat(result).isEqualTo(RoomCategory.PREMIUM);
+    }
+
+    @Test
+    void should_return_economy_category_when_customer_willing_to_pay_less_than_100_EUR() {
+        final RoomCategory result = objectUnderTest.getRoomCategory(99);
+
+        assertThat(result).isEqualTo(RoomCategory.ECONOMY);
+    }
+
+}
