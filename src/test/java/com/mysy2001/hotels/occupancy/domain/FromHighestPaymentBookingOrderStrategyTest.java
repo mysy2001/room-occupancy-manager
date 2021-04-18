cbf4f935ffce5fadecd8678703b4fbb7dc20d44c
Marcin Sokołowski <mysy2001@interia.pl>
2021-04-18 12:49:19 +0200

    added test

---
+++ src/test/java/com/mysy2001/hotels/occupancy/domain/FromHighestPaymentBookingOrderStrategyTest.java
@@ -0,0 +1,22 @@
+package com.mysy2001.hotels.occupancy.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class FromHighestPaymentBookingOrderStrategyTest {
+    @Test
+    void should_return_items_ordered_descending() {
+
+        final FromHighestPaymentBookingOrderStrategy objectUnderTest = new FromHighestPaymentBookingOrderStrategy();
+
+        final Collection<Integer> result = objectUnderTest.createBookingOrder(List.of(1, 2, 3, 4));
+
+        assertThat(result).containsExactly(4, 3, 2, 1);
+
+    }
+
+}
